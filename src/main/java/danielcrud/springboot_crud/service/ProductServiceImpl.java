242f9421c66b72a3de03577ce0f9a24a59e74518
drodrigor <danierrodrigo@gmail.com>
2026-04-10 14:37:55 +0200

    creación ProductServiceImpl

--- src/main/java/danielcrud/springboot_crud/service/ProductServiceImpl.java
+++ src/main/java/danielcrud/springboot_crud/service/ProductServiceImpl.java
@@ -1,8 +1,37 @@
 package danielcrud.springboot_crud.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import danielcrud.springboot_crud.entity.Product;
+
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService{
+
+    @Override
+    public void delete(Product product) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Product> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
