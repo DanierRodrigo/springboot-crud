512e6d06928b2f3d898a7432a024e8e194f6d480
drodrigor <danierrodrigo@gmail.com>
2026-04-10 14:46:40 +0200

    se instancia ProductRepository en ProductServiceImpl

--- src/main/java/danielcrud/springboot_crud/service/ProductServiceImpl.java
+++ src/main/java/danielcrud/springboot_crud/service/ProductServiceImpl.java
@@ -3,23 +3,30 @@ package danielcrud.springboot_crud.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import danielcrud.springboot_crud.entity.Product;
+import danielcrud.springboot_crud.repository.ProductRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository repository;
+    
+    @Transactional(readOnly = true)
+    @Override
+    public List<Product> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
     @Override
     public void delete(Product product) {
         // TODO Auto-generated method stub
         
-    }
-
-    @Override
-    public List<Product> findAll() {
-        // TODO Auto-generated method stub
-        return null;
     }
 
     @Override
