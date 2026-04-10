738cf44d31c05b14eb0970422fa053443d89b6ef
drodrigor <danierrodrigo@gmail.com>
2026-04-10 14:29:26 +0200

    definición ProductService

--- src/main/java/danielcrud/springboot_crud/service/ProductService.java
+++ src/main/java/danielcrud/springboot_crud/service/ProductService.java
@@ -1,5 +1,16 @@
 package danielcrud.springboot_crud.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import danielcrud.springboot_crud.entity.Product;
+
 public interface ProductService {
     
+    List<Product> findAll();
+    Optional<Product> findById(Long id);
+
+    Product save(Product product);
+
+    void delete(Product product);
 }
